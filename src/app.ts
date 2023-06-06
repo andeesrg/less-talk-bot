18fb8328d0c4f43470e56f31dff190b1e5c1aa40
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-06 17:10:35 +0300

    Initialized and launched the bot

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import { Telegraf } from 'telegraf';
+import LocalSession from 'telegraf-session-local';
+import { ConfigService, IConfigService } from '@config';
+import { Command, StartCommand, ClearCommand } from '@commands';
+import { IBotContext } from '@context';
+
+class Bot {
+	bot: Telegraf<IBotContext>;
+	commands: Command[] = [];
+
+	constructor(private readonly configService: IConfigService) {
+		this.bot = new Telegraf<IBotContext>(this.configService.get('BOT_TOKEN'));
+		this.bot.use(
+			new LocalSession({ database: 'sessions.json' }).middleware()
+		);
+	}
+
+	init() {
+		this.commands = [new StartCommand(this.bot), new ClearCommand(this.bot)];
+		for (const command of this.commands) {
+			command.handle();
+		}
+		this.bot.launch();
+	}
+}
+
+const bot = new Bot(new ConfigService());
+
+bot.init();
