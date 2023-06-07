7fca39c55c73ca60dd36fb75f3711d48d541c130
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-07 21:27:57 +0300

    Added "help" command

---
+++ src/commands/help.command.ts
@@ -0,0 +1,22 @@
+import { Telegraf } from 'telegraf';
+import { Command } from '@commands';
+import { IBotContext } from '@context';
+import { commands } from '@constants';
+
+export class HelpCommand extends Command {
+	constructor(bot: Telegraf<IBotContext>) {
+		super(bot);
+	}
+	handle(): void {
+		this.bot.command('help', ctx => {
+			ctx.reply(
+				`Here's the list of supported commands⬇️\n\n${commands
+					.map(
+						({ name, description, emoji }) =>
+							`${name} - ${description}${emoji}\n`
+					)
+					.join('')}`
+			);
+		});
+	}
+}
