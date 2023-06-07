ea8a6b4b7b55141bc3dadd785f253ca27f2b2b9c
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-07 21:27:23 +0300

    Added index file for better imports

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,3 +1,4 @@
 export { Command } from './command.class';
 export { StartCommand } from './start.command';
 export { ClearCommand } from './clear.command';
+export { HelpCommand } from './help.command';
