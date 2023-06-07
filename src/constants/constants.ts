879d8664d5f2f6755da5dc32b36ed0732cda14a5
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-07 21:27:42 +0300

    Added constants

---
+++ src/constants/constants.ts
@@ -0,0 +1,17 @@
+export const commands = [
+	{
+		name: '/help',
+		description: 'List of supported commands',
+		emoji: '🗒️',
+	},
+	{
+		name: '/start',
+		description: 'Start the conversation',
+		emoji: '👋🏼',
+	},
+	{
+		name: '/clear',
+		description: 'Clear the chat history',
+		emoji: '🧹',
+	},
+];
