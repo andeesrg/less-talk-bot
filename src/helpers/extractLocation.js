3576de4d7b4d3235154b41f4834f13d3a84cdc21
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-09 00:45:28 +0300

    Added function for extraction user location

---
+++ src/helpers/extractLocation.js
@@ -0,0 +1,11 @@
+import { locationRegex } from '@constants/regex';
+
+export const extractLocation = msg => {
+	const match = msg.match(locationRegex);
+	if (!match) return null;
+
+	const userLocation = match[0].split(',');
+	return {
+		city: userLocation[0],
+	};
+};
