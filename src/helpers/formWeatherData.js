37c749ac65a3f5a19903023094415acb0139a2fa
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-09 01:02:23 +0300

    Removed extra checking

--- src/helpers/formWeatherData.js
+++ src/helpers/formWeatherData.js
@@ -1,5 +1,4 @@
 export const formWeatherData = async data => {
-	if (typeof data === 'string') return data;
 	return `
    Here's your current weather forecast☀️\n\n${Object.entries(data)
 		.map(([key, value]) => `⚪️${key}: ${value}\n`)
