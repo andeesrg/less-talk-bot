377f6f63a2d6f65f5594d77b37ecacc6f0f51a12
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-09 00:44:19 +0300

    Moved commands to a detached file

---
+++ src/constants/commands.ts
@@ -0,0 +1,22 @@
+export const commands = [
+	{
+		name: '/help',
+		description: 'List of supported commands',
+		emoji: '🗒️',
+	},
+	{
+		name: '/start',
+		description: 'Start the conversation',
+		emoji: '👋🏼',
+	},
+	{
+		name: '/clear',
+		description: 'Clear the chat history',
+		emoji: '🧹',
+	},
+	{
+		name: '/weather',
+		description: 'Get the current weather',
+		emoji: '☁️',
+	},
+];
