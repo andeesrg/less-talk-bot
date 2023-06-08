753519708a0da86e6f7cf7e50d16999e3d27b97f
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-08 17:57:25 +0300

    Connected weather scene

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,29 @@
-import { Telegraf } from 'telegraf';
+import { Telegraf, Scenes } from 'telegraf';
 import LocalSession from 'telegraf-session-local';
 import { ConfigService, IConfigService } from '@config';
-import { Command, StartCommand, ClearCommand, HelpCommand } from '@commands';
+import {
+	Command,
+	StartCommand,
+	ClearCommand,
+	HelpCommand,
+	WeatherCommand,
+} from '@commands';
 import { IBotContext } from '@context';
+import { weather } from '@scenes';
 
 class Bot {
 	bot: Telegraf<IBotContext>;
 	commands: Command[] = [];
+	stage = new Scenes.Stage<Scenes.WizardContext>([weather]);
 
 	constructor(private readonly configService: IConfigService) {
 		this.bot = new Telegraf<IBotContext>(this.configService.get('BOT_TOKEN'));
 		this.bot.use(
-			new LocalSession({ database: 'sessions.json' }).middleware()
+			new LocalSession<IBotContext>({
+				database: 'sessions.json',
+			}).middleware()
 		);
+		this.bot.use(this.stage.middleware());
 	}
 
 	init() {
@@ -20,6 +31,7 @@ class Bot {
 			new StartCommand(this.bot),
 			new ClearCommand(this.bot),
 			new HelpCommand(this.bot),
+			new WeatherCommand(this.bot),
 		];
 		for (const command of this.commands) {
 			command.handle();
