a616c8f506474018dacb6abe6969543c3ff7bec3
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-13 10:38:04 +0300

    Typed scenes

--- src/scenes/cat.scene.ts
+++ src/scenes/cat.scene.ts
@@ -1,15 +1,13 @@
 import { Scenes } from 'telegraf';
 import { catApiUrl } from '@constants';
+import { IBotContext } from '@context';
 
-export const cat = new Scenes.WizardScene<Scenes.WizardContext>(
-	'cat',
-	async ctx => {
-		await ctx.reply('Looking for a random cat picture...');
-		try {
-			await ctx.replyWithPhoto({ url: catApiUrl });
-		} catch (e) {
-			return 'Oops the image was not found🙁';
-		}
-		return await ctx.scene.leave();
+export const cat = new Scenes.WizardScene<IBotContext>('cat', async ctx => {
+	await ctx.reply('Looking for a random cat picture...');
+	try {
+		await ctx.replyWithPhoto({ url: catApiUrl });
+	} catch (e) {
+		return 'Oops the image was not found🙁';
 	}
-);
+	return await ctx.scene.leave();
+});
--- src/scenes/dog.scene.ts
+++ src/scenes/dog.scene.ts
@@ -1,17 +1,15 @@
 import { Scenes } from 'telegraf';
 import { dogApiUrl } from '@constants';
 import axios from 'axios';
+import { IBotContext } from '@context';
 
-export const dog = new Scenes.WizardScene<Scenes.WizardContext>(
-	'dog',
-	async ctx => {
-		await ctx.reply('Looking for a random dog picture...');
-		try {
-			const { data } = await axios.get(dogApiUrl);
-			await ctx.replyWithPhoto({ url: data.message });
-		} catch (e) {
-			return 'Oops the image was not found🙁';
-		}
-		return await ctx.scene.leave();
+export const dog = new Scenes.WizardScene<IBotContext>('dog', async ctx => {
+	await ctx.reply('Looking for a random dog picture...');
+	try {
+		const { data } = await axios.get(dogApiUrl);
+		await ctx.replyWithPhoto({ url: data.message });
+	} catch (e) {
+		return 'Oops the image was not found🙁';
 	}
-);
+	return await ctx.scene.leave();
+});
