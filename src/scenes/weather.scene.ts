a4ceb1443e461ead6e03dbd5e3e51d9352bc45f6
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-15 01:48:45 +0300

    Rewrote scene

--- src/scenes/weather.scene.ts
+++ src/scenes/weather.scene.ts
@@ -1,45 +1,48 @@
-import { locationRegex, timeRegex } from "@constants/regex";
+import { confirmButtons, subButtons } from "@buttons";
+import { actions, locationRegex, timeRegex } from "@constants";
 import { IBotContext } from "@context";
 import { extractLocation, extractTime, formWeatherData } from "@helpers";
 import { SubscribeService, weatherService } from "@services";
-import { Composer, Markup, Scenes } from "telegraf";
+import { Composer, Scenes } from "telegraf";
 
 const locationHandler = new Composer<IBotContext>();
+const responseSubHandler = new Composer<IBotContext>();
 const timeHandler = new Composer<IBotContext>();
-const answerHandler = new Composer<IBotContext>();
+const responseConfirmTimeHandler = new Composer<IBotContext>();
 
 const enterHandler = async (ctx: IBotContext) => {
-	await ctx.replyWithMarkdownV2('Enter your location in format "*City*"');
 	ctx.session.chatId = ctx.message?.chat.id;
+	ctx.replyWithMarkdownV2('Enter city in format "*City*"');
 	return ctx.wizard.next();
 };
 
-locationHandler.on("text", async ctx => {
-	if (!locationRegex.test(ctx.message.text)) {
-		await ctx.reply("Wrong format!");
-		return await ctx.scene.reenter();
+locationHandler.hears(locationRegex, async ctx => {
+	ctx.session.userLocation = extractLocation(ctx.message.text);
+	ctx.reply("⌛️Wait a second please...");
+	try {
+		const data = await weatherService.getCurrWeather(
+			ctx.session.userLocation
+		);
+		await ctx.replyWithMarkdownV2(formWeatherData(data));
+	} catch (error) {
+		await ctx.reply("Oops something went wrong!Try again");
+		return ctx.scene.leave();
 	}
-
-	const userLocation = extractLocation(ctx.message.text);
-	ctx.session.userLocation = userLocation;
 	ctx.wizard.next();
 	if (typeof ctx.wizard.step === "function") {
 		return ctx.wizard.step(ctx, async () => {});
 	}
 });
-
-const requestHandler = async (ctx: IBotContext) => {
-	const userLocation = ctx.session.userLocation;
-	await ctx.reply("Looking for the weather forecast...");
-	try {
-		const weatherData = await weatherService.getCurrWeather(userLocation);
-		await ctx.replyWithMarkdownV2(formWeatherData(weatherData));
-	} catch {
-		await ctx.reply("Oops something went wrong!🙁");
-		return await ctx.scene.leave();
+locationHandler.on("text", async ctx => {
+	if (!locationRegex.test(ctx.message.text)) {
+		ctx.replyWithMarkdownV2(`*Wrong format\\!*`);
 	}
+});
+
+const suggestSubHandler = async (ctx: IBotContext) => {
 	await ctx.replyWithMarkdownV2(
-		`What time you'd like to receive a daily weather forecast?⏰\n\n_Please enter the time in format_  "*h/hh:mm*"`
+		"*Would you like to sub on a daily WF\\?*",
+		subButtons()
 	);
 	ctx.wizard.next();
 	if (typeof ctx.wizard.step === "function") {
@@ -47,54 +50,63 @@ const requestHandler = async (ctx: IBotContext) => {
 	}
 };
 
-timeHandler.on("text", async ctx => {
-	if (!timeRegex.test(ctx.message.text)) {
-		await ctx.replyWithMarkdownV2(
-			`_*Wrong format\\!*_ ❌\n\nEnter hours or mins in format *h/hh:mm*`
-		);
-		return ctx.wizard.selectStep(3);
-	}
-
-	const userRemindTime = extractTime(ctx.message.text);
-	ctx.session.userRemindTime = userRemindTime;
+responseSubHandler.action(actions.sub.action, async ctx => {
+	await ctx.editMessageText(
+		`*Enter notification time*⌚️\n\nEnter time in format *h/hh:mm*`,
+		{ parse_mode: "MarkdownV2" }
+	);
 	ctx.wizard.next();
 	if (typeof ctx.wizard.step === "function") {
 		return ctx.wizard.step(ctx, async () => {});
+	}
+});
+responseSubHandler.action(actions.dontSub.action, async ctx => {
+	await ctx.deleteMessage();
+	return ctx.scene.leave();
+});
+
+timeHandler.hears(timeRegex, async ctx => {
+	ctx.session.userSubTime = extractTime(ctx.message.text);
+	ctx.wizard.next();
+	if (typeof ctx.wizard.step === "function") {
+		return ctx.wizard.step(ctx, async () => {});
+	}
+});
+timeHandler.on("text", async ctx => {
+	if (!timeRegex.test(ctx.message.text)) {
+		ctx.replyWithMarkdownV2(`*Wrong format\\!*`);
 	}
 });
 
 const confirmTimeHandler = async (ctx: IBotContext) => {
-	await ctx.reply(
-		"Is this time correct?",
-		Markup.inlineKeyboard([
-			Markup.button.callback("✅Yes", "correctTime"),
-			Markup.button.callback("📝No, edit", "uncorrectTime"),
-		])
-	);
+	ctx.replyWithMarkdownV2(`Is this time correct?`, confirmButtons());
 	ctx.wizard.next();
 	if (typeof ctx.wizard.step === "function") {
 		return ctx.wizard.step(ctx, async () => {});
 	}
 };
 
-answerHandler.action("correctTime", async ctx => {
+responseConfirmTimeHandler.action(actions.confirmTime.action, async ctx => {
+	await ctx.deleteMessage();
+	ctx.reply(
+		`👀You've successfully subscribed on daily WF at ${ctx.session.userLocation}!`
+	);
 	const subscribe = new SubscribeService();
-	subscribe.setReminder();
-	await ctx.editMessageText("✅Great!Time is set");
-
+	await subscribe.activateSub();
 	return ctx.scene.leave();
 });
-answerHandler.action("uncorrectTime", async ctx => {
-	await ctx.editMessageText("📝Choose another time, please");
-	return ctx.wizard.selectStep(3);
+responseConfirmTimeHandler.action(actions.editTime.action, async ctx => {
+	ctx.replyWithMarkdownV2("Enter your new time in format *h/hh:mm*");
+	return ctx.wizard.selectStep(4);
 });
 
 export const weather = new Scenes.WizardScene<IBotContext>(
 	"weather",
 	enterHandler,
 	locationHandler,
-	requestHandler,
+	suggestSubHandler,
+	responseSubHandler,
 	timeHandler,
 	confirmTimeHandler,
-	answerHandler
+	responseConfirmTimeHandler
 );
