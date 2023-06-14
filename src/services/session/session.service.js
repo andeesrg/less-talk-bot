766333bb4672076097a41db8dbe37ee4f3ed79be
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-15 01:47:12 +0300

    Added light changes

--- src/services/session/session.service.js
+++ src/services/session/session.service.js
@@ -1,23 +1,23 @@
-import fs from 'fs';
+import fs from "fs";
 
 class SessionService {
 	readData() {
 		const data = fs.readFileSync(
-			'/Users/andreysergienko/less-talk-bot/sessions.json',
-			'utf-8'
+			"/Users/andreysergienko/less-talk-bot/sessions.json",
+			"utf-8"
 		);
 		return this.extractData(data);
 	}
 
 	extractData(fileData) {
 		const parsed = JSON.parse(fileData);
-		const sessions = parsed?.sessions;
-		const lastSession = sessions[sessions?.length - 1];
-		const { chatId, userLocation, userRemindTime } = lastSession.data;
+		const sessions = parsed.sessions;
+		const lastSession = sessions[sessions.length - 1];
+		const { chatId, userLocation, userSubTime } = lastSession.data;
 		return JSON.stringify({
 			chatId,
 			userLocation,
-			userRemindTime,
+			userSubTime,
 		});
 	}
 }
