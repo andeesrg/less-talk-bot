0ab18d018cf0132ea2351178269d77ed70361379
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-14 12:39:33 +0300

    Added extractTime helper

---
+++ src/helpers/extractTime.js
@@ -0,0 +1,10 @@
+import { timeRegex, minsRegex } from '@constants/regex';
+
+export const extractTime = msg => {
+	const match = msg.match(timeRegex);
+	if (!match) return null;
+
+	const [hours, minutes] = match[0].split(':');
+	const mins = minutes.match(minsRegex)[0];
+	return { hours, mins };
+};
