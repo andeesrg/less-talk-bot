eb00d4dd300fad35805abecdd93f2c128ef81d83
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-14 12:37:09 +0300

    Changed regex

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -1,1 +1,3 @@
-export const locationRegex = /^[A-Z]{1}[a-z]+$/gm;
+export const locationRegex = /^[A-Z]{1}[a-z]{4,}$/gm;
+export const timeRegex = /^([0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9])$/gm;
+export const minsRegex = /([0-9]|[1-5][0-9])$/gm;
