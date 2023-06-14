cbf1ee8ffc665a7afdd66e4de537d24507af0fe9
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-15 01:43:41 +0300

    Changed custom session interface

--- src/context/context.interface.ts
+++ src/context/context.interface.ts
@@ -1,9 +1,9 @@
-import { Context, Scenes } from 'telegraf';
+import { Context, Scenes } from "telegraf";
 
 interface MySession extends Scenes.WizardSession {
 	userLocation?: string;
 	chatId?: number;
-	userRemindTime?: { hours: string; mins: string } | null;
+	userSubTime?: { hours: string; mins: string } | null;
 }
 
 export interface IBotContext extends Context {
