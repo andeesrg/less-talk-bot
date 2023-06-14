7e669c90b28fa647b876dc30854307d385d9ea91
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-15 01:43:23 +0300

    Added actions for buttons

---
+++ src/constants/actions.ts
@@ -0,0 +1,18 @@
+export const actions = {
+	sub: {
+		text: "👀Sub",
+		action: "sub",
+	},
+	dontSub: {
+		text: "❌Don't sub",
+		action: "nonSub",
+	},
+	confirmTime: {
+		text: "☑️Correct",
+		action: "correctTime",
+	},
+	editTime: {
+		text: "📝Edit",
+		action: "editTime",
+	},
+};
