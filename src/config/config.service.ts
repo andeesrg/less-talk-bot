2c4795e6a34dcc00d817e23d265f2ddcaf3e9836
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-14 16:34:57 +0300

    Added light changes

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,17 +1,17 @@
-import { DotenvParseOutput, config } from 'dotenv';
-import { IConfigService } from './config.interface';
+import { DotenvParseOutput, config } from "dotenv";
+import { IConfigService } from "./config.interface";
 
 export class ConfigService implements IConfigService {
 	private config: DotenvParseOutput;
 	constructor() {
 		const { error, parsed } = config();
-		if (error) throw new Error('File .env is not found');
-		if (!parsed) throw new Error('File .env is empty');
+		if (error) throw new Error("File .env is not found");
+		if (!parsed) throw new Error("File .env is empty");
 		this.config = parsed;
 	}
 	get(key: string): string {
 		const res = this.config[key];
-		if (!res) throw new Error('No founds by this key');
+		if (!res) throw new Error("No founds by this key");
 		return res;
 	}
 }
