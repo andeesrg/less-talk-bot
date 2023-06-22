502a3a2c9d96b911b0209208f8d0bba6440e8c49
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-22 22:19:35 +0300

    Fixed bug with circular dependency

--- src/commands/cat.command.ts
+++ src/commands/cat.command.ts
@@ -1,12 +1,12 @@
-import { Telegraf } from 'telegraf';
-import { Command } from '@commands';
-import { IBotContext } from '@context';
+import { IBotContext } from "@context";
+import { Telegraf } from "telegraf";
+import { Command } from "./command.class";
 
 export class CatCommand extends Command {
 	constructor(bot: Telegraf<IBotContext>) {
 		super(bot);
 	}
 	handle(): void {
-		this.bot.command('cat', ctx => ctx.scene.enter('cat'));
+		this.bot.command("cat", ctx => ctx.scene.enter("cat"));
 	}
 }
