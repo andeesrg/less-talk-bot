ac89431b855b7fd78e71321b7bcf045312d1284e
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-27 23:43:57 +0300

    Added food scene

---
+++ src/scenes/guidance/food.scene.ts
@@ -0,0 +1,32 @@
+import { cityRegex } from "@constants";
+import { IBotContext } from "@context";
+import { Composer, Scenes } from "telegraf";
+
+const cityHandler = new Composer<IBotContext>();
+
+const enterCityHandler = async (ctx: IBotContext) => {
+	await ctx.replyWithMarkdownV2("Enter city in format *City*");
+
+	ctx.wizard.next();
+	if (typeof ctx.wizard.step === "function") {
+		return ctx.wizard.step(ctx, async () => {});
+	}
+};
+
+cityHandler.hears(cityRegex, async ctx => {
+	await ctx.reply("Gathering food...");
+	return ctx.scene.leave();
+});
+cityHandler.on("text", async ctx => {
+	if (!cityRegex.test(ctx.message.text)) {
+		await ctx.replyWithMarkdownV2(
+			"*City* is invalid ❌\nEnter city in proper format *City*"
+		);
+	}
+});
+
+export const food = new Scenes.WizardScene<IBotContext>(
+	"food",
+	enterCityHandler,
+	cityHandler
+);
