b18672defdbb59af44fb2e9a486817bf4e1e74fd
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-27 19:08:19 +0300

    Added method for deactivating subscription

--- src/services/subscribe/subscribe.service.ts
+++ src/services/subscribe/subscribe.service.ts
@@ -1,34 +1,50 @@
+import { unsubButton } from "@buttons";
 import { ConfigService } from "@config";
 import { IBotContext } from "@context";
-import { formWeatherData, parseDateNow } from "@helpers";
-import { sessionService, weatherService } from "@services";
+import { formWeatherForecast } from "@helpers";
+import { weatherService } from "@services";
 import cron from "node-cron";
 import { Telegraf } from "telegraf";
-import { ISubParams } from "./sub.interface";
 
 export class SubscribeService {
-	bot: Telegraf<IBotContext>;
-	private sessionData: ISubParams;
+	private bot: Telegraf<IBotContext>;
+	private currTask: any;
+	private static instance: SubscribeService;
 
-	constructor(private chatIdTerm: number) {
+	constructor(private chatIdTerm?: number) {
 		this.bot = new Telegraf<IBotContext>(
 			new ConfigService().get("BOT_TOKEN")
 		);
-		this.sessionData = JSON.parse(sessionService.readData(chatIdTerm));
 	}
-	async activateSub() {
+
+	async deactivateSub() {
+		await this.currTask?.stop();
+	}
+
+	async activateSub(subParams: any) {
 		const {
+			userSubLocation,
+			userSubTime: { hours, mins },
 			chatId,
-			userLocation,
-			userSubTime: { hours, mins } = parseDateNow(),
-		} = this.sessionData;
-		cron.schedule(`${mins} ${hours} * * *`, async () => {
-			const data = await weatherService.getCurrWeather(userLocation);
+		} = JSON.parse(subParams);
+		const currTask = cron.schedule(`${mins} ${hours} * * *`, async () => {
+			const data = await weatherService.getCurrWeather(userSubLocation);
 			new Telegraf(
 				new ConfigService().get("BOT_TOKEN")
-			).telegram.sendMessage(chatId, formWeatherData(data), {
+			).telegram.sendMessage(chatId, formWeatherForecast(data), {
 				parse_mode: "MarkdownV2",
+				...unsubButton(),
 			});
 		});
+		currTask.start();
+		this.currTask = currTask;
+	}
+
+	static getInstance(chatId?: number) {
+		if (!SubscribeService.instance) {
+			SubscribeService.instance = new SubscribeService(chatId);
+		}
+
+		return SubscribeService.instance;
 	}
 }
