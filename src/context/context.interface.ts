b0b28fc36873729cf5a2752837d5753a3f66b837
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-27 19:03:33 +0300

    Changed context data

--- src/context/context.interface.ts
+++ src/context/context.interface.ts
@@ -2,11 +2,12 @@ import { Context, Scenes } from "telegraf";
 
 interface MySceneSession extends Scenes.WizardSessionData {
 	taskId: number | string;
+	userLocation: string;
 }
 
 interface MySession extends Scenes.WizardSession<MySceneSession> {
-	userLocation?: string;
 	chatId: number;
+	userSubLocation?: string | null;
 	userSubTime?: { hours: string; mins: string } | null;
 }
 
