ee023fd0af1e4b106249c0a0e64d9fbe3c775dc5
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-27 23:45:47 +0300

    Added new helper

--- src/helpers/form/formUrl.js
+++ src/helpers/form/formUrl.js
@@ -1,5 +1,11 @@
-export const formGeocodingUrl = (location, apiKey) =>
+export const formWeatherGeoUrl = (location, apiKey) =>
 	`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${apiKey}`;
 
 export const formWeatherUrl = (coords, apiKey) =>
 	`https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}`;
+
+export const formCityGeoUrl = (location, apiKey) =>
+	`http://api.opentripmap.com/0.1/ru/places/geoname?name=${location}&apikey=${apiKey}`;
+
+export const formAttractionsApiUrl = (lat, lon, apiKey) =>
+	`http://api.opentripmap.com/0.1/ru/places/radius?radius=5000&lon=${lon}&lat=${lat}&kinds=interesting_places&limit=100&apikey=${apiKey}`;
