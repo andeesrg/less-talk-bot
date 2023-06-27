4244a199b5bbe5a6c9a39b1f366f81e9e3781abd
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-27 19:07:50 +0300

    Added some step handlers due to UX improvement

--- src/scenes/weather.scene.ts
+++ src/scenes/weather.scene.ts
@@ -1,14 +1,24 @@
-import { confirmButtons, subButtons } from "@buttons";
-import { cityRegex, subActions, timeRegex } from "@constants";
+import { confirmTimeButtons, subButtons, subCityButtons } from "@buttons";
+import {
+	cityRegex,
+	confirmTime,
+	editTime,
+	nonSub,
+	sub,
+	subCity,
+	timeRegex,
+} from "@constants";
 import { IBotContext } from "@context";
-import { extractCity, extractTime, formWeatherData } from "@helpers";
-import { SubscribeService, weatherService } from "@services";
+import { extractCity, extractTime, formWeatherForecast } from "@helpers";
+import { SubscribeService, sessionService, weatherService } from "@services";
 import { Composer, Scenes } from "telegraf";
 
 const cityHandler = new Composer<IBotContext>();
-const enterTimeHandler = new Composer<IBotContext>();
+const suggestSubCityHandler = new Composer<IBotContext>();
+const subCityHandler = new Composer<IBotContext>();
+const newSubCityHandler = new Composer<IBotContext>();
 const timeHandler = new Composer<IBotContext>();
-const enterConfirmTimeHandler = new Composer<IBotContext>();
+const confirmTimeHandler = new Composer<IBotContext>();
 
 const enterCityHandler = async (ctx: IBotContext) => {
 	ctx.replyWithMarkdownV2("🏙️Enter city in format *City*");
@@ -16,17 +26,18 @@ const enterCityHandler = async (ctx: IBotContext) => {
 };
 
 cityHandler.hears(cityRegex, async ctx => {
-	ctx.session.userLocation = extractCity(ctx.message.text);
-	ctx.reply("☀️Receiving weather...");
+	ctx.scene.session.userLocation = extractCity(ctx.message.text);
+	await ctx.reply("☀️Receiving weather...");
 	try {
 		const data = await weatherService.getCurrWeather(
-			ctx.session.userLocation
+			ctx.scene.session.userLocation
 		);
-		await ctx.replyWithMarkdownV2(formWeatherData(data));
+		await ctx.replyWithMarkdownV2(formWeatherForecast(data));
 	} catch {
 		await ctx.reply("Oops something went wrong🤕!Try again later");
 		return ctx.scene.leave();
 	}
+
 	ctx.wizard.next();
 	if (typeof ctx.wizard.step === "function") {
 		return ctx.wizard.step(ctx, async () => {});
@@ -41,8 +52,8 @@ cityHandler.on("text", async ctx => {
 });
 
 const suggestSubHandler = async (ctx: IBotContext) => {
-	await ctx.replyWithMarkdownV2(
-		"☀️*Would you like to receive daily weather forecast\\?*",
+	await ctx.reply(
+		"☀️Would you like to receive daily weather forecast?",
 		subButtons()
 	);
 
@@ -52,10 +63,10 @@ const suggestSubHandler = async (ctx: IBotContext) => {
 	}
 };
 
-enterTimeHandler.action(subActions.sub.action, async ctx => {
-	await ctx.editMessageText(
-		`*Enter notification time*⌚️\n\nEnter time in format *h/hh:mm*`,
-		{ parse_mode: "MarkdownV2" }
+suggestSubCityHandler.action(sub.action, async ctx => {
+	await ctx.reply(
+		"Choose a city you'd like to get weather about🏙️",
+		subCityButtons()
 	);
 
 	ctx.wizard.next();
@@ -63,10 +74,56 @@ enterTimeHandler.action(subActions.sub.action, async ctx => {
 		return ctx.wizard.step(ctx, async () => {});
 	}
 });
-enterTimeHandler.action(subActions.dontSub.action, async ctx => {
+suggestSubCityHandler.action(nonSub.action, async ctx => {
 	await ctx.deleteMessage();
 	return ctx.scene.leave();
 });
+
+subCityHandler.action(subCity.currCity.action, async ctx => {
+	ctx.session.userSubLocation = ctx.scene.session.userLocation;
+
+	ctx.wizard.selectStep(6);
+	if (typeof ctx.wizard.step === "function") {
+		return ctx.wizard.step(ctx, async () => {});
+	}
+});
+subCityHandler.action(subCity.newCity.action, async ctx => {
+	await ctx.editMessageText("🏙️Enter city in format *City*", {
+		parse_mode: "MarkdownV2",
+	});
+
+	ctx.wizard.next();
+	if (typeof ctx.wizard.step === "function") {
+		return ctx.wizard.step(ctx, async () => {});
+	}
+});
+
+newSubCityHandler.hears(cityRegex, async ctx => {
+	ctx.session.userSubLocation = ctx.message.text;
+
+	ctx.wizard.next();
+	if (typeof ctx.wizard.step === "function") {
+		return ctx.wizard.step(ctx, async () => {});
+	}
+});
+newSubCityHandler.on("text", async ctx => {
+	if (!cityRegex.test(ctx.message.text)) {
+		await ctx.replyWithMarkdownV2(
+			"*City* is invalid ❌\nEnter city in proper format *City*"
+		);
+	}
+});
+
+const enterTimeHandler = async (ctx: IBotContext) => {
+	await ctx.replyWithMarkdownV2(
+		`*Enter notification time*⌚️\n\nEnter time in format *h/hh:mm*\\(7:05\\)`
+	);
+
+	ctx.wizard.next();
+	if (typeof ctx.wizard.step === "function") {
+		return ctx.wizard.step(ctx, async () => {});
+	}
+};
 
 timeHandler.hears(timeRegex, async ctx => {
 	ctx.session.userSubTime = extractTime(ctx.message.text);
@@ -79,13 +136,13 @@ timeHandler.hears(timeRegex, async ctx => {
 timeHandler.on("text", async ctx => {
 	if (!timeRegex.test(ctx.message.text)) {
 		await ctx.replyWithMarkdownV2(
-			"*Time* is invalid❌\\!\nEnter time in proper format\\!(e.g. 7:00)"
+			"*Time* is invalid❌\\!\nEnter time in proper format\\!(e.g. 7:05)"
 		);
 	}
 });
 
-const confirmTimeHandler = async (ctx: IBotContext) => {
-	ctx.replyWithMarkdownV2(`Is this time correct\\?⌚️`, confirmButtons());
+const enterConfirmTimeHandler = async (ctx: IBotContext) => {
+	await ctx.reply("Is this time correct?⌚️", confirmTimeButtons());
 
 	ctx.wizard.next();
 	if (typeof ctx.wizard.step === "function") {
@@ -93,18 +150,19 @@ const confirmTimeHandler = async (ctx: IBotContext) => {
 	}
 };
 
-enterConfirmTimeHandler.action(subActions.confirmTime.action, async ctx => {
+confirmTimeHandler.action(confirmTime.action, async ctx => {
 	await ctx.deleteMessage();
-	const subscribe = new SubscribeService(ctx.session.chatId);
-	await subscribe.activateSub();
-	ctx.reply(
-		`👀You've subscribed on daily weather forecast at ${ctx.session.userLocation}!`
+	const subscribe = SubscribeService.getInstance(ctx.session.chatId);
+	const subParams = sessionService.readData(ctx.session.chatId);
+	await subscribe.activateSub(subParams);
+	await ctx.reply(
+		`👀You've subscribed on daily weather forecast in ${ctx.session.userSubLocation}!`
 	);
 	return ctx.scene.leave();
 });
-enterConfirmTimeHandler.action(subActions.editTime.action, async ctx => {
-	ctx.replyWithMarkdownV2("Enter time in format *h/hh:mm*⌚️");
-	return ctx.wizard.selectStep(4);
+confirmTimeHandler.action(editTime.action, async ctx => {
+	await ctx.replyWithMarkdownV2("Enter time in format *h/hh:mm*⌚️");
+	return ctx.wizard.selectStep(7);
 });
 
 export const weather = new Scenes.WizardScene<IBotContext>(
@@ -112,8 +170,11 @@ export const weather = new Scenes.WizardScene<IBotContext>(
 	enterCityHandler,
 	cityHandler,
 	suggestSubHandler,
+	suggestSubCityHandler,
+	subCityHandler,
+	newSubCityHandler,
 	enterTimeHandler,
 	timeHandler,
-	confirmTimeHandler,
-	enterConfirmTimeHandler
+	enterConfirmTimeHandler,
+	confirmTimeHandler
 );
