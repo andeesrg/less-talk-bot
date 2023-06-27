c812f45e0ea4a71b86dfdc0ab63cd94de5fc8eb3
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-27 19:06:52 +0300

    Added scene for unsubscribing

---
+++ src/scenes/unsubscribe.scene.ts
@@ -0,0 +1,19 @@
+import { IBotContext } from "@context";
+import { SubscribeService } from "@services";
+import { Scenes } from "telegraf";
+
+const unsubHandler = async (ctx: IBotContext) => {
+	const subscribe = SubscribeService.getInstance();
+	await subscribe.deactivateSub();
+	ctx.session.userSubLocation = null;
+	ctx.session.userSubTime = { hours: "", mins: "" };
+	await ctx.reply(
+		"You've succesfully unsubscribed from daily weather forecast✅"
+	);
+	return ctx.scene.leave();
+};
+
+export const unsubscribe = new Scenes.WizardScene<IBotContext>(
+	"unsubscribe",
+	unsubHandler
+);
