478374f803d10b9dfccd7da809b4d15794d37436
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-27 19:02:35 +0300

    Added button for unsubscribing

--- src/buttons/buttons.js
+++ src/buttons/buttons.js
@@ -1,46 +1,43 @@
-import { subActions, taskActions, taskEditActions } from "@constants";
+import {
+	confirmTime,
+	editTime,
+	nonSub,
+	sub,
+	subCity,
+	taskEdit,
+	tasks,
+	unsub,
+} from "@constants/actions";
 import { Markup } from "telegraf";
 
 export const subButtons = () =>
 	Markup.inlineKeyboard([
-		Markup.button.callback(subActions.sub.text, subActions.sub.action),
-		Markup.button.callback(
-			subActions.dontSub.text,
-			subActions.dontSub.action
-		),
+		Markup.button.callback(sub.text, sub.action),
+		Markup.button.callback(nonSub.text, nonSub.action),
 	]);
 
-export const confirmButtons = () =>
+export const unsubButton = () =>
+	Markup.inlineKeyboard([Markup.button.callback(unsub.text, unsub.action)]);
+
+export const subCityButtons = () =>
 	Markup.inlineKeyboard([
-		Markup.button.callback(
-			subActions.confirmTime.text,
-			subActions.confirmTime.action
-		),
-		Markup.button.callback(
-			subActions.editTime.text,
-			subActions.editTime.action
-		),
+		Markup.button.callback(subCity.currCity.text, subCity.currCity.action),
+		Markup.button.callback(subCity.newCity.text, subCity.newCity.action),
+	]);
+
+export const confirmTimeButtons = () =>
+	Markup.inlineKeyboard([
+		Markup.button.callback(confirmTime.text, confirmTime.action),
+		Markup.button.callback(editTime.text, editTime.action),
 	]);
 
 export const tasksControlButtons = () =>
 	Markup.keyboard(
 		[
-			Markup.button.callback(
-				taskActions.createTask.text,
-				taskActions.createTask.action
-			),
-			Markup.button.callback(
-				taskActions.readTasks.text,
-				taskActions.readTasks.action
-			),
-			Markup.button.callback(
-				taskActions.editTask.text,
-				taskActions.editTask.action
-			),
-			Markup.button.callback(
-				taskActions.removeTask.text,
-				taskActions.removeTask.action
-			),
+			Markup.button.callback(tasks.create.text, tasks.create.action),
+			Markup.button.callback(tasks.read.text, tasks.read.action),
+			Markup.button.callback(tasks.edit.text, tasks.edit.action),
+			Markup.button.callback(tasks.remove.text, tasks.remove.action),
 		],
 		{
 			columns: 2,
@@ -49,12 +46,6 @@ export const tasksControlButtons = () =>
 
 export const taskEditButtons = () =>
 	Markup.inlineKeyboard([
-		Markup.button.callback(
-			taskEditActions.title.text,
-			taskEditActions.title.action
-		),
-		Markup.button.callback(
-			taskEditActions.status.text,
-			taskEditActions.status.action
-		),
+		Markup.button.callback(taskEdit.title.text, taskEdit.title.action),
+		Markup.button.callback(taskEdit.status.text, taskEdit.status.action),
 	]);
