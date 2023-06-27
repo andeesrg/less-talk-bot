8dcbd97dd57495335c7364799d1b795dada7dce4
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-27 19:04:19 +0300

    Refactored formWeatherData helper

--- src/helpers/form/formWeatherData.js
+++ src/helpers/form/formWeatherData.js
@@ -1,7 +1,21 @@
-export const formWeatherData = data => {
-	return `
-   *Here's your current weather forecast*☀️\n\n${Object.entries(data)
-		.map(([key, value]) => `⚪️ *${key}*: ${value}\n`)
-		.join('')}
-   `;
-};
+import { convertToCelcius } from "@helpers/convert";
+import { capitalizeWord } from "@helpers/simple";
+
+export function formWeatherData(city, data) {
+	const condition = data.weather[0].description;
+	const main = data.main;
+	const visibility = (data.visibility / 1000).toFixed(0);
+	const windSpeed = data.wind.speed.toFixed(0);
+
+	return {
+		city,
+		condition: capitalizeWord(condition),
+		temperature: convertToCelcius(main.temp),
+		feels: convertToCelcius(main.feels_like),
+		min: convertToCelcius(main.temp_min),
+		max: convertToCelcius(main.temp_max),
+		visibility: `${visibility} km`,
+		["wind speed"]: `${windSpeed} mps`,
+		humidity: main.humidity + "%",
+	};
+}
