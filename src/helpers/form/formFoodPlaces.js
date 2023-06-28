727253169325c5fdc563da9ece8b7e2b2fcc0957
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-28 13:22:33 +0300

    Added helper for forming food places view

---
+++ src/helpers/form/formFoodPlaces.js
@@ -0,0 +1,18 @@
+import { genRandomIdx, genRandomRate } from "@helpers/simple";
+
+export const formFoodPlaces = data => {
+	const randomIdx = genRandomIdx(data.result.length);
+
+	return `
+      🗿Here're some popular food places in <b>${data.city}</b>\n\n${data.result
+		.slice(randomIdx, Math.min(randomIdx + 5, data.result.length))
+		.map(
+			({ name, rating }) =>
+				`🍿<b>Name</b>: <i>${name}</i>\n⭐️<b>Rating</b>: <i>${genRandomRate(
+					3,
+					8
+				)}/10</i>`
+		)
+		.join("\n\n")}
+   `;
+};
