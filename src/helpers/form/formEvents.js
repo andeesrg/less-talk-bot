2b73f4d43281bbccf2033cfc8cba857e3d84df17
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-28 13:22:21 +0300

    Added helper for forming events view

---
+++ src/helpers/form/formEvents.js
@@ -0,0 +1,19 @@
+import { extractDate } from "@helpers/extract";
+import { genRandomIdx } from "@helpers/simple";
+
+export const formEvents = data => {
+	const randomIdx = genRandomIdx(data.result.length);
+
+	return `
+      🗿Here're some events in <b>${data.city}</b>\n\n${data.result
+		.slice(randomIdx, Math.min(randomIdx + 5, data.result.length))
+		.map(
+			({ name, date, day }) =>
+				`🎃<b>Title</b>: <i>${name}</i>\n⏱️<b>When</b>: <i>${extractDate(
+					date,
+					day
+				)}</i>`
+		)
+		.join("\n\n")}
+   `;
+};
