af9fdd9160dafbc41c627950f71589dc60dfa9bf
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-28 13:19:31 +0300

    Added new API urls

--- src/helpers/form/formUrl.js
+++ src/helpers/form/formUrl.js
@@ -8,4 +8,10 @@ export const formCityGeoUrl = (location, apiKey) =>
 	`http://api.opentripmap.com/0.1/ru/places/geoname?name=${location}&apikey=${apiKey}`;
 
 export const formAttractionsApiUrl = (lat, lon, apiKey) =>
-	`http://api.opentripmap.com/0.1/ru/places/radius?radius=5000&lon=${lon}&lat=${lat}&kinds=interesting_places&limit=100&apikey=${apiKey}`;
+	`http://api.opentripmap.com/0.1/en/places/radius?radius=5000&lon=${lon}&lat=${lat}&kinds=interesting_places&limit=100&apikey=${apiKey}`;
+
+export const formFoodApiUrl = (lat, lon, apiKey) =>
+	`https://api.opentripmap.com/0.1/en/places/radius?radius=5000&lon=${lon}&lat=${lat}&kinds=foods&limit=100&apikey=${apiKey}`;
+
+export const formEventsApiUrl = countryCode =>
+	`https://api.api-ninjas.com/v1/holidays?country=${countryCode}&year=${new Date().getFullYear()}`;
