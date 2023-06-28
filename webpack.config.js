1d912f823e6dac1d00f37ad511241c56d24eaec5
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-28 18:23:38 +0300

    Added webpack settings

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+	mode: "development",
+	entry: "./src/app.ts",
+	output: {
+		filename: "index.js",
+		path: path.resolve(__dirname, "build"),
+		library: {
+			type: "module",
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-typescript"],
+					},
+				},
+			},
+		],
+	},
+	resolve: {
+		plugins: [new TsconfigPathsPlugin()],
+		extensions: [".tsx", ".ts", ".js"],
+		alias: {
+			"@config": path.resolve(__dirname, "src/config/index"),
+			"@context": path.resolve(__dirname, "src/context/index"),
+			"@commands": path.resolve(__dirname, "src/commands/index"),
+			"@constants": path.resolve(__dirname, "src/constants/index"),
+			"@constants/*": path.resolve(__dirname, "src/constants/*"),
+			"@helpers": path.resolve(__dirname, "src/helpers/index"),
+			"@helpers/*": path.resolve(__dirname, "src/helpers/*"),
+			"@services": path.resolve(__dirname, "src/services/index"),
+			"@services/*": path.resolve(__dirname, "src/services/*"),
+			"@scenes": path.resolve(__dirname, "src/scenes/index"),
+			"@buttons": path.resolve(__dirname, "src/buttons/index"),
+		},
+	},
+	experiments: {
+		outputModule: true,
+	},
+	externals: [nodeExternals()],
+};
