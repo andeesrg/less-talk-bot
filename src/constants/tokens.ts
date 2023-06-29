be3f78f2863e3f9e128d766dc8dff956c5daece2
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-29 16:52:49 +0300

    Added light changes

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -4,8 +4,8 @@ config();
 
 export const tokens = {
 	botToken: process.env.BOT_TOKEN || "",
-	weatherToken: process.env.WEATHER_TOKEN || "",
-	dbToken: process.env.DB_URL || "",
+	weatherApiToken: process.env.WEATHER_API_KEY || "",
+	dbUrl: process.env.DB_URL || "",
 	openTripToken: process.env.OPENTRIP_API_KEY || "",
 	eventsApiToken: process.env.EVENTS_API_KEY || "",
 };
