481300d7abd12567da1667f154b46450e8ebd5cf
Andrey <andrey.sergienko.dev@gmail.com>
2023-06-30 10:57:43 +0300

    Added light changes

--- src/helpers/extract/extractTime.js
+++ src/helpers/extract/extractTime.js
@@ -2,7 +2,7 @@ import { extractMinsRegex, timeRegex } from "@constants/regex";
 
 export const extractTime = msg => {
 	const match = msg.match(timeRegex);
-	if (!match) return null;
+	if (!match) return { hours: "", mins: "" };
 
 	const [hours, minutes] = match[0].split(":");
 	const mins = minutes.match(extractMinsRegex)[0];
