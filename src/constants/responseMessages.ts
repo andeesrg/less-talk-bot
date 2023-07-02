3946bf34d579eecfe6b937b67830f5163195f36e
Andrey <andrey.sergienko.dev@gmail.com>
2023-07-02 15:11:54 +0300

    review: Moved static messages to constants

--- src/constants/responseMessages.ts
+++ src/constants/responseMessages.ts
@@ -1,4 +1,4 @@
 export const badAttractions = "Oops attractions not found🤕";
 export const badFoodPlaces = "Oops food places not found🤕";
 export const badEvents = "Oops events not found🤕";
-export const badWeather = "Oops something went wrong"
+export const badWeather = "Oops city is not found🤕";
