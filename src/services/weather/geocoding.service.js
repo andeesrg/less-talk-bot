0b4c19f8056824e95ea7beac468eb735b432a396
Andrey <andrey.sergienko.dev@gmail.com>
2023-07-02 17:31:11 +0300

    review: Moved static messages to constants

--- src/services/weather/geocoding.service.js
+++ src/services/weather/geocoding.service.js
@@ -1,4 +1,4 @@
-import { badCity } from "@constants";
+import { notFound } from "@constants";
 import { formWeatherGeoUrl } from "@helpers";
 import axios from "axios";
 
@@ -6,15 +6,15 @@ class GeocodingService {
 	async getCoordinates(location, apiKey) {
 		const { data } = await axios.get(formWeatherGeoUrl(location, apiKey));
 
-		if (!data?.length) return { geoData: null, error: badCity };
+		if (!data?.length) return { geoData: null, error: notFound.city };
 
 		const extractedData = this.#extractData(await data);
 		return { geoData: extractedData, error: null };
 	}
 
 	#extractData(data) {
-		const { lat, lon, name } = data[0];
-		return { lat, lon, name };
+		const { lat, lon, name, country } = data[0];
+		return { lat, lon, name, country };
 	}
 }
 
