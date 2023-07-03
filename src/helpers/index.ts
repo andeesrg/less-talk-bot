65c40f54db27da733dc6d6d0f1ac80bb4e598e8a
Andrey <andrey.sergienko.dev@gmail.com>
2023-07-03 13:25:58 +0300

    Removed useless helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,7 +2,5 @@ export * from "./convert";
 export * from "./extract";
 export * from "./form";
 export * from "./order";
-export * from "./parse";
 export * from "./simple";
 export * from "./transform";
-
