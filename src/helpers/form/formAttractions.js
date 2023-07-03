02dd17ed2da8e0b2f2a6216f80ded6f96f93d154
Andrey <andrey.sergienko.dev@gmail.com>
2023-07-03 18:38:14 +0300

    Added some code-styles changes

--- src/helpers/form/formAttractions.js
+++ src/helpers/form/formAttractions.js
@@ -1,11 +1,11 @@
 import { genRandomIdx, genRandomRate } from "@helpers/simple";
 
 export const formAttractions = data => {
-	const random = genRandomIdx(data.result.length);
+	const randomIdx = genRandomIdx(data.result.length);
 
 	return `
       🗿Here're some popular places in <b>${data.city}</b>\n\n${data.result
-		.slice(random, Math.min(random + 5, data.result.length))
+		.slice(randomIdx, Math.min(randomIdx + 5, data.result.length))
 		.map(
 			({ name, rating }) =>
 				`🌆<b>Name</b>: <i>${name}</i>\n⭐️<b>Rate</b>: <i>${genRandomRate(
