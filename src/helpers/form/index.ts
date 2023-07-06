e4562246679b71bf495cd2e2013cee0b3c4e75c2
Andrey <andrey.sergienko.dev@gmail.com>
2023-07-07 00:52:36 +0300

    Removed extra

--- src/helpers/form/index.ts
+++ src/helpers/form/index.ts
@@ -2,5 +2,4 @@ export { formAttractions } from "./formAttractions";
 export { formEvents } from "./formEvents";
 export { formFoodPlaces } from "./formFoodPlaces";
 export { formTasks } from "./formTasks";
-export * from "./formUrl";
 export { formWeatherForecast } from "./formWeatherForecast";
